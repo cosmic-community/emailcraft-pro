3497e0d9bd0b983b1635db776b5a54baf9055571
Cosmic AI <support@cosmicjs.com>
2025-08-17 14:36:45 -0700

    edit lib/cosmic.ts

--- lib/cosmic.ts
+++ lib/cosmic.ts
@@ -4,7 +4,8 @@ import { Contact, EmailTemplate, Campaign, CosmicResponse } from '@/types'
 const cosmic = createBucketClient({
   bucketSlug: process.env.COSMIC_BUCKET_SLUG as string,
   readKey: process.env.COSMIC_READ_KEY as string,
-  writeKey: process.env.COSMIC_WRITE_KEY as string
+  writeKey: process.env.COSMIC_WRITE_KEY as string,
+  apiEnvironment: 'staging'
 })
 
 // Contact functions
